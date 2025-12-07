238ac0be50415ceca984cc5ff1c020e5ea4b1e69
Buster Coden <busterc246@proton.me>
2025-12-07 01:34:47 -0500

    Added restore_backup script and switched firewall script over to using prebuilt one

--- linux/blue_team/firewall.sh
+++ linux/blue_team/firewall.sh
@@ -1,22 +1,2 @@
 #!/bin/sh
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
-
-iptables -N INPUT_ACCEPT
-iptables -N OUTPUT_ACCEPT
-
-iptables -N INPUT_DROP
-iptables -N OUTPUT_DROP
-
-iptables -A INPUT_ACCEPT -j LOG --log-prefix "[INPUT_ACCEPT]"
-iptables -A INPUT_ACCEPT -j ACCEPT
-
-iptables -A OUTPUT_ACCEPT -j LOG --log-prefix "[OUTPUT_ACCEPT]"
-iptables -A OUTPUT_ACCEPT -j ACCEPT
-
-iptables -A OUTPUT_DROP -j LOG --log-prefix "[OUTPUT_DROP]"
-iptables -A OUTPUT_DROP -j DROP
-
-iptables -A INPUT_DROP -j LOG --log-prefix "[INPUT_DROP]"
-iptables -A INPUT_DROP -j DROP
+./iptables_script/iptables_UML/5-firewall-iptables.sh
---
+++ linux/blue_team/restore_backup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+BACKUP_FILE="$1"
+
+if [ -z "$BACKUP_FILE" ]; then
+  echo "Usage: $0 <path_to_backup_file>"
+  exit 1
+fi
+
+if [ ! -f "$BACKUP_FILE" ]; then
+  echo "Error: File '$BACKUP_FILE' not found."
+  exit 1
+fi
+
+tar -xzpf "$BACKUP_FILE" -C /
