9dd236ce83d1c4f48945bb703f44ada259f8bf14
Buster Coden <busterc246@proton.me>
2025-12-09 18:39:05 -0500

    More initial wazuh setup

--- linux/blue_team/setup_wazuh_server.sh
+++ linux/blue_team/setup_wazuh_server.sh
@@ -23,3 +23,24 @@ if command -v apt-get; then
 
   curl -s https://packages.wazuh.com/4.x/filebeat/wazuh-filebeat-0.4.tar.gz | tar -xvz -C /usr/share/filebeat/module
 fi
+
+if command -v dnf; then
+  rpm --import https://packages.wazuh.com/key/GPG-KEY-WAZUH
+
+  echo -e '[wazuh]\ngpgcheck=1\ngpgkey=https://packages.wazuh.com/key/GPG-KEY-WAZUH\nenabled=1\nname=EL-$releasever - Wazuh\nbaseurl=https://packages.wazuh.com/4.x/yum/\npriority=1' | tee /etc/yum.repos.d/wazuh.repo
+
+  dnf -y install wazuh-manager filebeat
+
+  curl -so /etc/filebeat/filebeat.yml https://packages.wazuh.com/4.14/tpl/wazuh/filebeat/filebeat.yml
+
+  filebeat keystore create
+
+  echo admin | filebeat keystore add username --stdin --force
+  echo admin | filebeat keystore add password --stdin --force
+
+  curl -so /etc/filebeat/wazuh-template.json https://raw.githubusercontent.com/wazuh/wazuh/v4.14.1/extensions/elasticsearch/7.x/wazuh-template.json
+
+  chmod go+r /etc/filebeat/wazuh-template.json
+
+  curl -s https://packages.wazuh.com/4.x/filebeat/wazuh-filebeat-0.4.tar.gz | tar -xvz -C /usr/share/filebeat/module
+fi
