90aeaf7df2e5e5d13cee28fc6f4aaeffa1f6a904
justinzztang <justinzztang@gmail.com>
2026-01-18 21:44:31 -0500

    Add files via upload

---
+++ linux/services/wordpress_scan.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ "$(id -u)" -eq 0 ]; then
+  echo "DONT run as root"
+  exit 1
+fi
+
+# Download and install WP-CLI
+sudo apt install curl -y
+sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+sudo chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+
+
+# Download and use WP vulnerability scanner
+wp package install 10up/wpcli-vulnerability-scanner:dev-stable
+sudo echo  "define( 'VULN_API_PROVIDER', 'wordfence');" >> /var/www/html/wp-config.php
+echo "manually use 'wp core/plugin verify-checksums' and 'wp user list' in the wordpress directory"
+echo "manually use 'wp vuln status' in the wordpress directory"
