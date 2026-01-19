1f4af6c7d5f085bb1768b69e5ae4623fc7c93544
Bill Nguyen <billnguyen080306@gmail.com>
2026-01-18 17:15:04 -0800

    remove copy

--- hehe-osint/old/folderintegrity.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:g
-dir /S /B \Windows\system32 > new.txt
-fc new.txt orig.txt
-timeout /t 5 > NUL
-cls
-goto g
--- hehe-osint/old/goldentickets.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-:g
-set rand=%RANDOM%
-net user krbtgt PASSWORD!@#%rand% > NUL
-net user krbtgt PASSWORD!@#%rand%
-timeout /t 120 > NUL
-cls
-goto g
